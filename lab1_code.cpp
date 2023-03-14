3641b6287b1904b8d1b8dfd94d8f76adf6dc90df
Ivan <60141137+untibullet@users.noreply.github.com>
2023-03-14 18:01:58 +0300

    Add files via upload

---
+++ lab1_code.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float A, B, C, D;
+
+    cout << "Enter A, B, C, B:" << endl;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+
+    float multiplier1, multiplier2, multiplier3;
+
+    multiplier1 = 1 / pow(A, 2);
+    multiplier2 = pow(D / 10, 3);
+    multiplier3 = pow(C + D, 2);
+
+    float output = multiplier1 * multiplier2 * multiplier3;
+
+    cout << output;
+
+}
