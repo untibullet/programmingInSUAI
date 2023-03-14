ac01d521a8654945f62a1250e01f212884b56343
Ivan <60141137+untibullet@users.noreply.github.com>
2023-03-14 20:57:27 +0300

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Entry X:" << endl;
+
+	float x;
+	cin >> x;
+
+	double output;
+	if (x > 4.5) {
+		output = 2 * x;
+	}
+	else if (x < 0) {
+		output = exp(-1 * x);
+	}
+	else {
+		output = 1 - log(fabs(1 - pow(x, 2)));
+	}
+
+	cout << output << endl;
+}
---
+++ lab3.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <math.h>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "");
+
+    cout << "Введите кол-во копеек:" << endl;
+    int number;
+    cin >> number;
+
+    if (11 <= number && number <= 14) {
+        printf("%d %s", number, "копеек");
+        return 0;
+    }
+
+    int remainder = number % 10;
+
+    if (number > 10 && remainder == 1) {
+        printf("%d %s", number, "копейка");
+        return 0;
+    }
+
+    switch (remainder) {
+    case 0:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+        printf("%d %s", number, "копеек");
+        break;
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        printf("%d %s", number, "копейки");
+        break;
+    }
+}
